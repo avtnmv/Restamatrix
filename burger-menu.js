419aa310c98f7da7e4bd615edc009667d32fcc81
Yaroslav Avtonomov <avtnmv@icloud.com>
2024-11-26 00:24:30 +0200

    feat: add no-scroll class for burger menu

--- burger-menu.js
+++ burger-menu.js
@@ -6,7 +6,14 @@ const toggleMenu = () => {
     const show = !burgerMenu.classList.contains('show');
     burgerMenu.classList.toggle('show', show);
     burgerIcon.classList.toggle('active', show);
+
+    document.body.classList.toggle('no-scroll', show);
 };
 
 burgerIcon.addEventListener('click', toggleMenu);
-burgerLinks.forEach(link => link.addEventListener('click', () => toggleMenu(false)));
+
+burgerLinks.forEach(link => link.addEventListener('click', () => {
+    burgerMenu.classList.remove('show');
+    burgerIcon.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+}));
